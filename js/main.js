8262d677309b1568c43b3995065117a886834eb1
Ojitos1735 <anyg_1735@hotmail.com>
2023-05-28 13:23:07 -0300

    front-home

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
+// const nav = document.querySelector("#nav");
+// const abrir = document.querySelector("#abrir");
+// const cerrar = document.querySelector("#cerrar");
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
+// abrir.addEventListener("click", () => {
+//     nav.classList.add("visible");
+// })
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
+// cerrar.addEventListener("click", () => {
+//     nav.classList.remove("visible");
+// })
+
+// Obtener elementos del DOM
+var modal = document.getElementById("myModal");
+var btnOpen = document.getElementById("openModal");
+var btnClose = document.getElementsByClassName("close")[0];
+
+// Abrir modal al hacer clic en el botón de apertura
+btnOpen.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Cerrar modal al hacer clic en el botón de cierre
+btnClose.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cerrar modal al hacer clic fuera del contenido del modal
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
